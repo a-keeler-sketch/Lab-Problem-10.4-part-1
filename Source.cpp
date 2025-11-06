fa5abf42540fa0bebcd44f355359be07cf06499f
Anna Keeler <a-keeler@onu.edu>
2025-11-06 13:53:21 -0500

    Completed P10-4 Part

--- Source.cpp
+++ Source.cpp
@@ -11,36 +11,42 @@
 #include <cmath>
 using namespace std;
 int main(void) {
-	int value;
-	int digit;
-	int digit2;
+	while (true) {
+		
+		int value;
+		int digit;
+		int digit2;
 
-	int digit3;
-	int digit4;
-	int sum1 = 0;
-	int sum2 = 0;
-	int verify;
+		int digit3;
+		int digit4;
+		int sum1 = 0;
+		int sum2 = 0;
+		int verify;
 
-	cout << "Please enter 8-digit card number: ";
-	cin >> value;
-	while (value > 0) {
-		digit = (value % 10);
-		value = value / 10;
-		digit2 = (value % 10);
-		sum1 += digit;
-		value = value / 10;
-		digit2 *= 2;
-		while (digit2 > 0) {
-			digit3 = (digit2 % 10);
-			digit2 = digit2 / 10;
-			sum2 += digit3;
+		cout << "Please enter 8-digit card number: ";
+		cin >> value;
+		if (cin.fail()) {
+			break;
 		}
+		while (value > 0) {
+			digit = (value % 10);
+			value = value / 10;
+			digit2 = (value % 10);
+			sum1 += digit;
+			value = value / 10;
+			digit2 *= 2;
+			while (digit2 > 0) {
+				digit3 = (digit2 % 10);
+				digit2 = digit2 / 10;
+				sum2 += digit3;
+			}
 
 
+		}
+		verify = sum2 + sum1;
+		if (verify % 10 == 0) cout << "Card is valid." << endl;
+		else cout << " Card is invalid" << endl;
 	}
-	verify = sum2 + sum1;
-	if (verify % 10 == 0) cout << "Card is valid.";
-	else cout << " Card is invalid";
 
 	return 0;
 
